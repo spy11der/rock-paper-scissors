1cd16d253b21a6eac0d62cdc3ddfd68490b7b08c
spy11der <spy11der@Xubuntu.myguest.virtualbox.org>
2026-01-18 11:41:15 -0800

    Add first step of get computer choice

---
+++ rockps.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    let randomNumber = Math.random();
+
+    if (randomNumber < 0.33) {
+        return "rock";
+    } else if (randomNumber < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
