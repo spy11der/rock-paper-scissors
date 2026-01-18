4eeae7d97c5809b8af87052110c8a14af9010631
spy11der <spy11der@Xubuntu.myguest.virtualbox.org>
2026-01-18 14:15:09 -0800

    Fix human and computer score section

--- rockps.js
+++ rockps.js
@@ -19,37 +19,45 @@ function getHumanChoice() {
 }
 console.log(getHumanChoice());
 
-let humanScore, computerScore;
-humanScore = 0;
-computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-function playRound(humanChoice, computerChoice); {
+function playRound(humanChoice, computerChoice) {
    if (humanChoice === computerChoice) {
        return "It's a tie!";
    }
    // Human loses cases
    if (humanChoice === "rock" && computerChoice === "paper") { 
+    computerScore++;
     return "You lose! Paper beats rock.";
    }
 
    if (humanChoice === "paper" && computerChoice === "scissors") {
+    computerScore++;
     return "You lose! Scissors beats paper.";
    }
 
    if (humanChoice === "scissors" && computerChoice === "rock") {
+    computerScore++;
     return "You lose! Rock beats scissors.";
    }
 
    // Human wins cases
    if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore++;
     return "You win! Rock beats scissors.";
    }
 
    if (humanChoice === "paper" && computerChoice === "rock") {
+    humanScore++;
     return "You win! Paper beats rock.";
    }
 
    if (humanChoice === "scissors" && computerChoice === "paper") {
+    humanScore++;
     return "You win! Scissors beats paper.";
    }
 }
+
+let result = playRound(humanScore, computerScore);
+
